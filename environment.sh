7fd0262f2d350499911c89acbadb900567644efb
orkhanfarmanli <orkhanfarmanli@gmail.com>
2017-01-02 12:12:13 +0400

    added sudo to dpgk command

--- environment.sh
+++ environment.sh
@@ -2,4 +2,4 @@
 
 #Testing sublime text download and install
 
-wget https://download.sublimetext.com/sublime-text_build-3126_amd64.deb -O sublime.deb && dpkg -i sublime.deb
+wget https://download.sublimetext.com/sublime-text_build-3126_amd64.deb -O sublime.deb && sudo dpkg -i sublime.deb
