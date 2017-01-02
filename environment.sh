b4ca759f23d742689ac41c7da25dc9c8ecdd8d8a
Orkhan Farmanli <orkhanfarmanli@gmail.com>
2017-01-02 13:02:39 +0400

    Added Atom editor

--- environment.sh
+++ environment.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
-#Testing sublime text download and install
+# Create a "downloads" folder
 
-wget https://download.sublimetext.com/sublime-text_build-3126_amd64.deb -O sublime.deb && sudo dpkg -i sublime.deb
+mkdir downloads
+
+
+# Download deb files
+
+wget https://download.sublimetext.com/sublime-text_build-3126_amd64.deb -O downloads/sublime.deb
+wget https://atom.io/download/deb -O downloads/atom.deb
+
+
+# Entering downloads folder and installing everything with a loop
+
+cd downloads
+
+PROGRAMS=$(ls *deb)
+
+for PROGRAM in $PROGRAMS
+do
+  sudo dpkg -i ${PROGRAM}
+done
