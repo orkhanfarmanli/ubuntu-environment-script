7f75468114941ec20944d172ae2f9654634b7ca9
orkhanfarmanli <orkhanfarmanli@gmail.com>
2017-01-02 11:57:45 +0400

    A little change on sublime install

--- environment.sh
+++ environment.sh
@@ -1,5 +1,5 @@
-# sudo apt -y install vlc browser-plugin-vlc
-# wget https://download.sublimetext.com/sublime-text_build-3126_amd64.deb https://atom.io/download/deb
-wget https://download.sublimetext.com/sublime-text_build-3126_amd64.deb -O sublime.deb
-# wget https://atom.io/download/deb -O atom.deb
-dpkg -i sublime.deb
+#!/bin/bash
+
+#Testing sublime text download and install
+
+wget https://download.sublimetext.com/sublime-text_build-3126_amd64.deb -O sublime.deb && dpkg -i sublime.deb
