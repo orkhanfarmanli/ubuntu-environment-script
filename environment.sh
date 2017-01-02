b1fa86fe025379bf51f4833cecaf25bd799e5a03
orkhanfarmanli <orkhanfarmanli@gmail.com>
2017-01-02 11:20:58 +0400

    Initial commit

---
+++ environment.sh
@@ -0,0 +1,5 @@
+# sudo apt -y install vlc browser-plugin-vlc
+# wget https://download.sublimetext.com/sublime-text_build-3126_amd64.deb https://atom.io/download/deb
+wget https://download.sublimetext.com/sublime-text_build-3126_amd64.deb -O sublime.deb
+# wget https://atom.io/download/deb -O atom.deb
+dpkg -i sublime.deb
