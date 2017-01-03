4f5495db09f33fc488184c1824c41a180f1cf684
orkhanfarmanli <orkhanfarmanli@gmail.com>
2017-01-03 12:46:00 +0400

    Added wmail

--- install.sh
+++ install.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 # Create a "downloads" folder
-
 mkdir downloads
 
 
 # Download deb files
-
 wget https://download.sublimetext.com/sublime-text_build-3126_amd64.deb -O downloads/sublime.deb
 wget https://atom.io/download/deb -O downloads/atom.deb
 wget https://downloads.slack-edge.com/linux_releases/slack-desktop-2.3.4-amd64.deb -O downloads/slack.deb
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O downloads/chrome.deb
+wget https://github.com/Thomas101/wmail/releases/download/v2.0.0/WMail_2_0_0_linux_x86_64.deb -O downloads/wmail.deb
 
-# Entering downloads folder and installing everything with a loop
 
+# Entering downloads folder and installing everything (.deb files) with a loop
 cd downloads
 
 PROGRAMS=$(ls *deb)
@@ -24,11 +23,10 @@ do
 done
 
 
+############################################################################################
+
 # Installing apps  from repositories
 sudo apt update
-
-
-############################################################################################
 
 # Add the Spotify repository signing key to be able to verify downloaded packages
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 && echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
@@ -39,10 +37,12 @@ sudo add-apt-repository ppa:ricotz/docky #
 # Adapta theme repository
 sudo add-apt-repository ppa:tista/adapta
 
-############################################################################################
-
 # Update
 sudo apt update
+
+
+############################################################################################
+
 
 # 4. Install Spotify
 sudo apt install -y spotify-client gnome-tweak-tool plank adapta-gtk-theme virtualbox git-core
@@ -54,9 +54,9 @@ sudo apt upgrade
 sudo apt autoremove
 
 
-# Custom installations
-
 ############################################################################################
+
+# Custom installations
 
 #Flat-Remix icon theme
 
